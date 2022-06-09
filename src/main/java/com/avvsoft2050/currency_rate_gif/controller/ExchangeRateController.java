8008aa25cce70a9d2f1cbeb743adf236f4a6ed94
Anton2050 <avvsoft2050@gmail.com>
2022-06-09 17:50:50 +0300

    add ExchangeRateController

---
+++ src/main/java/com/avvsoft2050/currency_rate_gif/controller/ExchangeRateController.java
@@ -0,0 +1,23 @@
+package com.avvsoft2050.currency_rate_gif.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/v1/rates")
+public class ExchangeRateController {
+
+    private final GetUrlFromGiphyService getUrlFromGiphyService;
+
+    public ExchangeRateController(GetUrlFromGiphyService getUrlFromGiphyService) {
+        this.getUrlFromGiphyService = getUrlFromGiphyService;
+    }
+
+    @GetMapping(value = "{symbols}")
+    public ResponseEntity<String> getGifByExchangeRateRep(@PathVariable String symbols) {
+        return getUrlFromGiphyService.responseFromGiphy(symbols);
+    }
+}
