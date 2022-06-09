d7dca982b8489c8eae296ff48d73a9eb99fe7774
Anton2050 <avvsoft2050@gmail.com>
2022-06-09 17:45:21 +0300

    Initial commit;

---
+++ src/main/java/com/avvsoft2050/currency_rate_gif/CurrencyRateGifApplication.java
@@ -0,0 +1,13 @@
+package com.avvsoft2050.currency_rate_gif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyRateGifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyRateGifApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avvsoft2050/currency_rate_gif/CurrencyRateGifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avvsoft2050.currency_rate_gif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyRateGifApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
